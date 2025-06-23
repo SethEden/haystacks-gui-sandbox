6a1103f9c895c2cd2ce183fbf157dcd5e6896e17
SethEden <Iceversaka@hotmail.com>
2025-06-22 19:31:49 -0500

    main: Continue with setting up the bootstrap process.

---
+++ test/testHarness/src/resources/constantsValidation/application.configuration.constants.validation.js
@@ -0,0 +1,23 @@
+/**
+ * @file application.configuration.constants.validation.js
+ * @module application.configuration.constants.validation
+ * @description Contains all validations for named application configuration constants.
+ * @requires module:application.configuration.constants
+ * @author Seth Hollingsead
+ * @date 2025/06/22
+ * @copyright Copyright © 2025-… by Seth Hollingsead. All rights reserved
+ */
+
+// Internal imports
+import * as app_cfg from '../../constants/application.configuration.constants.js';
+
+/**
+ * @function applicationConfigurationConstantsValidation
+ * @description Initializes the application configuration constants validation data objects array.
+ * @return {array<Object<NamedCurve,Actual,Expected>>} An array of constants validation data objects.
+ * @author Seth Hollingsead
+ * @date 2025/06/22
+ */
+export const applicationConfigurationConstantsValidation = [
+  {Name: 'cargumentDrivenInterface', Actual: app_cfg.cargumentDrivenInterface, Expected: 'argumentDrivenInterface'}
+];
--- test/testHarness/src/testHarness.js
+++ test/testHarness/src/testHarness.js
@@ -21,67 +21,145 @@
 
 // Internal imports
 import { app, BrowserWindow, screen } from 'electron';
-// import * as app_cfg from './constants/application.configuration.constants.js';
-// import * as apc from './constants/application.constants.js';
-// import * as app_msg from './constants/application.message.constants.js';
-// import allAppCV from './resources/constantsValidation/allApplicationConstantsValidationMetadata.js';
-// // External imports
-// import main from '../../../src/main.js';
-// import hayConst from '@haystacks/constants';
-// import url from 'url';
-// import dotenv from 'dotenv';
-// import path from 'path';
+import * as app_cfg from './constants/application.configuration.constants.js';
+import * as apc from './constants/application.constants.js';
+import * as app_msg from './constants/application.message.constants.js';
+import allAppCV from './resources/constantsValidation/allApplicationConstantsValidationMetadata.js';
+// External imports
+import main from '../../../src/main.js';
+import hayConst from '@haystacks/constants';
+import url from 'url';
+import dotenv from 'dotenv';
+import path from 'path';
 
-// const {bas, biz, cmd, msg, sys, wrd} = hayConst;
-// let rootPath = '';
-// let baseFileName = path.basename(import.meta.url, path.extname(import.meta.url));
-// // application.testHarness.
-// let namespacePrefix = wrd.capplication + bas.cDot + baseFileName + bas.cDot;
-// // eslint-disable-next-line no-undef
-// global.appRoot = path.resolve(process.cwd());
-// dotenv.config();
-// // eslint-disable-next-line no-undef
-// const {NODE_ENV} = process.env;
-// let exitConditionArrayIndex = 0;
+const {bas, biz, cmd, msg, sys, wrd} = hayConst;
+let rootPath = '';
+let baseFileName = path.basename(import.meta.url, path.extname(import.meta.url));
+// application.testHarness.
+let namespacePrefix = wrd.capplication + bas.cDot + baseFileName + bas.cDot;
+// eslint-disable-next-line no-undef
+global.appRoot = path.resolve(process.cwd());
+dotenv.config();
+// eslint-disable-next-line no-undef
+const {NODE_ENV} = process.env;
+let exitConditionArrayIndex = 0;
 
-// /**
-//  * @function bootstrapApplication
-//  * @description Setup all the testHarness application data and configuration settings.
-//  * @return {void}
-//  * @author Seth Hollingsead
-//  * @date 2025/06/19
-//  */
-// async function bootstrapApplication() {
-//   let functionName = bootstrapApplication.name;
-//   console.log(`BEGIN ${namespacePrefix}${functionName} function`);
-//   rootPath = url.fileURLToPath(path.dirname(import.meta.url));
-//   let rootPathArray = [];
-//   let pathSeparator = '';
-//   if (rootPath.includes(bas.cBackSlash) === true) {
-//     pathSeparator = bas.cBackSlash;
-//   } else if (rootPath.includes(bas.cForwardSlash) === true) {
-//     pathSeparator = bas.cForwardSlash;
-//   }
-//   rootPathArray = rootPath.split(pathSeparator);
-//   rootPathArray.pop(); // remove any bin or src folder from the path.
-//   rootPath = rootPathArray.join(pathSeparator);
-//   let appConfig = {};
-//   if (NODE_ENV === wrd.cdevelopment) {
+/**
+ * @function bootstrapApplication
+ * @description Setup all the testHarness application data and configuration settings.
+ * @return {void}
+ * @author Seth Hollingsead
+ * @date 2025/06/19
+ */
+async function bootstrapApplication() {
+  const functionName = bootstrapApplication.name;
+  console.log(`BEGIN ${namespacePrefix}${functionName} function`);
+  rootPath = url.fileURLToPath(path.dirname(import.meta.url));
+  let rootPathArray = [];
+  let pathSeparator = '';
+  if (rootPath.includes(bas.cBackSlash) === true) {
+    pathSeparator = bas.cBackSlash;
+  } else if (rootPath.includes(bas.cForwardSlash) === true) {
+    pathSeparator = bas.cForwardSlash;
+  }
+  rootPathArray = rootPath.split(pathSeparator);
+  rootPathArray.pop(); // remove any bin or src folder from the path.
+  rootPath = rootPathArray.join(pathSeparator);
+  let appConfig = {};
+  if (NODE_ENV === wrd.cdevelopment) {
+    appConfig = {
+      FrameworkName: apc.cExpectedActualFrameworkDevName,
+      clientRootPath: rootPath,
+      appConfigResourcesPath: rootPath + apc.cFullDevResourcesPath,
+      appConfigReferencePath: rootPath + apc.cFullDevConfigurationPath,
+      clientMetaDataPath: apc.cmetaDataDevPath,
+      clientCommandAliasesPath: rootPath + apc.cFullDevCommandsPath,
+      clientConstantsPath: rootPath + apc.cFullDevConstantsPath,
+      clientRegisteredPlugins: rootPath + apc.cFullDevPluginsRegistryPath,
+      clientWorkflowsPath: rootPath + apc.cFullDevWorkflowsPath,
+      clientThemesPath: rootPath + apc.cFullDevThemesPath,
+      applicationConstantsValidationData: allAppCV.initializeAllClientConstantsValidationData,
+      clientBusinessRules: {},
+      clientCommands: {}
+    };
+  } else if (NODE_ENV === wrd.cproduction) {
+    appConfig = {
+      FrameworkName: apc.cExpectedActualFrameworkProdName,
+      clientRootPath: rootPath,
+      appConfigResourcesPath: rootPath + apc.cFullProdResourcesPath,
+      appConfigReferencePath: rootPath + apc.cFullProdConfigurationPath,
+      clientMetaDataPath: apc.cmetaDataProdPath,
+      clientCommandAliasesPath: rootPath + apc.cFullProdCommandsPath,
+      clientConstantsPath: rootPath + apc.cFullProdConstantsPath,
+      clientRegisteredPlugins: rootPath + apc.cFullProdPluginsRegistryPath,
+      clientWorkflowsPath: rootPath + apc.cFullProdWorkflowsPath,
+      clientThemesPath: rootPath + apc.cFullProdThemesPath,
+      applicationConstantsValidationData: allAppCV.initializeAllClientConstantsValidationData,
+      clientBusinessRules: {},
+      clientCommands: {}
+    };
+  } else {
+    // WARNING: No .env file found! Going to default to the DEVELOPMENT ENVIRONMENT!
+    console.log(msg.cApplicationWarningMessage1a + msg.cApplicationWarningMessage1b);
+    appConfig = {
+      FrameworkName: apc.cExpectedActualFrameworkDevName,
+      clientRootPath: rootPath,
+      appConfigResourcesPath: rootPath + apc.cFullDevResourcesPath,
+      appConfigReferencePath: rootPath + apc.cFullDevConfigurationPath,
+      clientMetaDataPath: apc.cmetaDataDevPath,
+      clientCommandAliasesPath: rootPath + apc.cFullDevCommandsPath,
+      clientConstantsPath: rootPath + apc.cFullDevConstantsPath,
+      clientRegisteredPlugins: rootPath + apc.cFullDevPluginsRegistryPath,
+      clientWorkflowsPath: rootPath + apc.cFullDevWorkflowsPath,
+      clientThemesPath: rootPath + apc.cFullDevThemesPath,
+      applicationConstantsValidationData: allAppCV.initializeAllClientConstantsValidationData,
+      clientBusinessRules: {},
+      clientCommands: {}
+    };
+  }
+  // appConfig[sys.cclientBusinessRules] = await clientRules.initClientRulesLibrary();
+  // appConfig[sys.cclientCommands] = await clientCommands.initClientCommandsLibrary();
+  console.log('appConfig is: ', appConfig);
+  console.log(`END ${namespacePrefix}${functionName} function`);
+}
 
-//   } else if (NODE_ENV === wrd.cproduction) {
+/**
+ * @function applicationInit
+ * @description This is the application startup orchestration function.
+ * Manages the bootstrapping of the haystacks command environment, and calls the function to create the main application window.
+ * Calls any additional processing as part of the startup process.
+ * @return {void}
+ * @author Seth Hollingsead
+ * @date 2025/06/22
+ */
+async function applicationInit() {
+  const functionName = applicationInit.name;
+  console.log(`BEGIN ${namespacePrefix}${functionName} function`);
+  try {
+    // 1. Wait for all bootstrapping to complete
+    await bootstrapApplication();
 
-//   } else {
+    // 2. Only now create the main application window
+    await createWindow();
+  } catch (error) {
+    // ERROR: Fatal error during bootstrap: 
+    console.log(app_msg.cErrorFatalBootstrap, error);
+  }
+  console.log(`END ${namespacePrefix}${functionName} function`);
+}
 
-//   }
-//   appConfig[sys.cclientBusinessRules] = await clientRules.initClientRulesLibrary();
-//   appConfig[sys.cclientCommands] = await clientCommands.initClientCommandsLibrary();
-//   console.log('appConfig is: ', appConfig);
-//   console.log(`END ${namespacePrefix}${functionName} function`);
-// }
+/**
+ * @function createWindow
+ * @description This creates the main application window.
+ * @return {void}
+ * @author Seth Hollingsead
+ * @date 2025/06/22
+ */
+async function createWindow() {
+  let functionName = createWindow.name;
+  console.log(`BEGIN ${namespacePrefix}${functionName} function`);
 
-const createWindow = () => {
-  // let functionName = createWindow.name;
-  // console.log(`BEGIN ${namespacePrefix}${functionName} function`);
+  bootstrapApplication();
 
   const displays = screen.getAllDisplays();
   let displayAsOneWindow = false;
@@ -178,7 +256,7 @@ const createWindow = () => {
 
   }
 
-  // console.log(`END ${namespacePrefix}${functionName} function`);
+  console.log(`END ${namespacePrefix}${functionName} function`);
 };
 
-app.whenReady().then(createWindow);
+app.whenReady().then(applicationInit);
